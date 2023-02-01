822affdc0633b81b1db4f9a40a106868d484a945
Behruz <behruzbahtiyorov10@gmail.com>
2023-02-01 22:47:27 +0500

    that's all

--- script.js
+++ script.js
@@ -54,26 +54,3 @@ function handleVolume(e) {
 function showHideKeys() {
   pianoKeys.forEach((key) => key.classList.toggle("hide"));
 }
-
-// others
-
-window.onresize = function () {
-  let sizes = JSON.parse(localStorage.getItem("sizes"));
-  if (sizes.h != window.innerHeight || sizes.w != window.innerWidth) {
-    window.location.href =
-      "https://i.pinimg.com/originals/a1/9f/09/a19f09386c8bf4a5ba48d07a7b4171f2.gif";
-  }
-};
-
-window.onload = function () {
-  let h = window.innerHeight,
-    w = window.innerWidth;
-  localStorage.setItem("sizes", JSON.stringify({ h, w }));
-
-  let sizes = JSON.parse(localStorage.getItem("sizes"));
-
-  if (h != window.innerHeight || w != window.innerWidth) {
-    window.location.href =
-      "https://i.pinimg.com/originals/a1/9f/09/a19f09386c8bf4a5ba48d07a7b4171f2.gif";
-  }
-};
