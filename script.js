48d83e6130dfaaec7fd51e2bd86bc47738fc4e7d
Behruz <behruzbahtiyorov10@gmail.com>
2023-02-01 22:07:17 +0500

    fixed sound

--- script.js
+++ script.js
@@ -4,51 +4,47 @@ const volumeSlider = document.querySelector(".volume-silder input");
 const keysCheckbox = document.querySelector(".keys-checkbox input");
 
 // Variables
-let allKeys = []
-let audio = new Audio()
-audio.playbackRate = 0.5
+let allKeys = [];
+let audio = new Audio();
+audio.playbackRate = 0.5;
 
 pianoKeys.forEach((key) => {
-    allKeys.push(key.dataset.key)
-    audio.pause()
+  allKeys.push(key.dataset.key);
+  audio.pause();
   key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
 // Events
 
-keysCheckbox.addEventListener("click" , showHideKeys)
-volumeSlider.addEventListener("input" , handleVolume)
-document.addEventListener("keydown" , pressedKey)
+keysCheckbox.addEventListener("click", showHideKeys);
+volumeSlider.addEventListener("input", handleVolume);
+document.addEventListener("keydown", pressedKey);
 
 // Functions
 
 function playTune(key) {
-    if(!audio.src){
-        audio.src= "audios/" + key + ".mp3"
-        console.log(key);
-    }
+  audio.src = "audios/" + key + ".mp3";
 
-    audio.play()
+  audio.play();
 
+  const clickedKey = document.querySelector(`[data-key=${key}]`);
 
-    const clickedKey = document.querySelector(`[data-key=${key}]`)
-    
-    clickedKey.classList.add("active")
+  clickedKey.classList.add("active");
 
-    setTimeout(() => {
-        clickedKey.classList.remove("active")
-    }, 150)
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
 }
 
 function pressedKey(e) {
-    audio.pause()
-    allKeys.includes(e.key) && playTune(e.key)
+  audio.pause();
+  allKeys.includes(e.key) && playTune(e.key);
 }
 
 function handleVolume(e) {
-    audio.volume = e.target.value;
+  audio.volume = e.target.value;
 }
 
 function showHideKeys() {
-    pianoKeys.forEach(key => key.classList.toggle("hide"))
+  pianoKeys.forEach((key) => key.classList.toggle("hide"));
 }
